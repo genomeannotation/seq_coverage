cc06e37c1ae161ea264681b5fd255a06feb63b3a
Theodore DeRego <tderego94@gmail.com>
2014-10-06 16:55:35 -1000

    yay coverage stuff

--- fastq_coverage.py
+++ fastq_coverage.py
@@ -1,27 +1,44 @@
 #!/usr/bin/env python
 
 import sys
+from os import listdir
+from os.path import isfile, join
+from collections import Counter
 
-from src.fastq import read_fastq
+from src.fastq import read_fastq, get_seq_coverage, get_avg_seq_coverage
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: fastq_coverage.py <file.fastq>")
-    
-    fastq_path = sys.argv[1]
+    #if len(sys.argv) < 2:
+    #    print("Usage: fastq_coverage.py <file.fastq>")
+    #    exit()
 
-    with open(fastq_path, "r") as fastq_file:
-        seqs = read_fastq(fastq_file)
+    male_path = "data/M"
+    female_path = "data/F"
 
-        coverage = {}
-        for seq in seqs:
-            if seq.bases in coverage:
-                coverage[seq.bases] += 1
-            else:
-                coverage[seq.bases] = 1
-        
-        for seq, count in coverage.items():
-            print(seq+"\t"+str(count))
+    male_files = [join(male_path, f) for f in listdir(male_path) if isfile(join(male_path, f))]
+    female_files = [join(female_path, f) for f in listdir(female_path) if isfile(join(female_path, f))]
+
+    # Calculate coverage
+    male_coverage = Counter()
+    for male_file in male_files:
+        print("Processing "+male_file+" ...")
+        with open(male_file, "r") as fastq_file:
+            seqs = read_fastq(fastq_file)
+            male_coverage += Counter(get_seq_coverage(seqs))
+    female_coverage = Counter()
+    for female_file in female_files:
+        print("Processing "+female_file+" ...")
+        with open(female_file, "r") as fastq_file:
+            seqs = read_fastq(fastq_file)
+            female_coverage += Counter(get_seq_coverage(seqs))
+
+    # What seqs to keep?
+    kept_male_seqs = []
+    avg_male_coverage = get_avg_seq_coverage(male_coverage)
+    for seq, count in male_coverage.items():
+        if count >= avg_male_coverage/3 and count <= avg_male_coverage/2:
+            kept_male_seqs.append(seq)
+            print(seq)
 
 #############################
 
--- src/fastq.py
+++ src/fastq.py
@@ -28,3 +28,15 @@ def read_fastq(io_buffer):
 def translate_scores(scorestring):
     """Translates a string of phred scores to a list of integers"""
     return [score_as_int(score) for score in scorestring]
+
+def get_seq_coverage(seqs):
+    coverage = {}
+    for seq in seqs:
+        if seq.bases in coverage:
+            coverage[seq.bases] += 1
+        else:
+            coverage[seq.bases] = 1
+    return coverage
+
+def get_avg_seq_coverage(coverage):
+    return sum(coverage.values())/len(coverage.values())
